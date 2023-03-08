a504b373a51bf48a31e7a87153c4060749fe7391
larestrepo <larestrepomu@gmail.com>
2023-03-07 18:45:37 -0600

    api routes connecting with mongodb

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,27 +1,28 @@
-import classes from './newsletter-registration.module.css';
+import classes from "./newsletter-registration.module.css";
 import { useRef, useState } from "react";
 
 function NewsletterRegistration() {
   const [message, setMessage] = useState("");
   const emailRef = useRef();
+
   function registrationHandler(event) {
     event.preventDefault();
     const email = emailRef.current.value;
     if (!email) {
-      return setMessage("Please send a valid email")
+      return setMessage("Please send a valid email");
     }
 
-    const emailData = { email: email }
+    const emailData = { email: email };
 
-    fetch('/api/events', {
-      method: 'POST',
+    fetch("/api/newsletter", {
+      method: "POST",
       body: JSON.stringify(emailData),
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
     })
-    .then((repsonse) => repsonse.json())
-    .then((data) => console.log(data))
+      .then((repsonse) => repsonse.json())
+      .then((data) => console.log(data));
 
     // fetch user input (state or refs)
     // optional: validate input
@@ -29,27 +30,26 @@ function NewsletterRegistration() {
   }
 
   if (!message) {
-  return (
+    return (
+      <section className={classes.newsletter}>
+        <h2>Sign up to stay updated!</h2>
+        <form onSubmit={registrationHandler}>
+          <div className={classes.control}>
+            <input
+              type="email"
+              id="email"
+              placeholder="Your email"
+              aria-label="Your email"
+              ref={emailRef}
+            />
+            <button>Register</button>
+          </div>
+        </form>
+      </section>
+    );
+  }
 
-    <section className={classes.newsletter}>
-      <h2>Sign up to stay updated!</h2>
-      <form onSubmit={registrationHandler}>
-        <div className={classes.control}>
-          <input
-            type='email'
-            id='email'
-            placeholder='Your email'
-            aria-label='Your email'
-            ref={emailRef}
-          />
-          <button>Register</button>
-        </div>
-      </form>
-    </section>
-  );}
-
-  return ( <p>{message}</p>
-  );
+  return <p>{message}</p>;
 }
 
 export default NewsletterRegistration;
--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,7 +1,7 @@
 import { Fragment, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
-import { getFilteredEvents } from "../../helpers/db";
+// import { getFilteredEvents } from "../../helpers/api-utils";
 import EventList from "../../components/events/event-list";
 import ResultsTitle from "../../components/events/results-title";
 import Button from "../../components/ui/button";
@@ -139,23 +139,25 @@ function FilteredEventsPage() {
   const numYear = +filteredYear;
   const numMonth = +filteredMonth;
 
-  if (isNaN(numYear) ||
+  if (
+    isNaN(numYear) ||
     isNaN(numMonth) ||
     numYear > 2030 ||
     numYear < 2021 ||
     numMonth < 1 ||
-    numMonth > 12 || error
+    numMonth > 12 ||
+    error
   ) {
-  return (
-    <Fragment>
-      <ErrorAlert>
-        <p>Invalid filter. Please adjust your values!</p>
-      </ErrorAlert>
-      <div className="center">
-        <Button link="/events">Show All Events</Button>
-      </div>
-    </Fragment>
-  );
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid filter. Please adjust your values!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
   }
 
   const filteredEvents = LoadedEvents.filter((event) => {
@@ -170,8 +172,6 @@ function FilteredEventsPage() {
   //   year: numYear,
   //   month: numMonth,
   // });
-
-
 
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
